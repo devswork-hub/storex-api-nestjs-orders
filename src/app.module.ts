44ab804b1d9b16c3083cd1d206d52e7016bc7120
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-06-07 23:51:46 -0300

    chore: init architecture with mongoose and graphql

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nestjst-mongodb-graphql'),
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
