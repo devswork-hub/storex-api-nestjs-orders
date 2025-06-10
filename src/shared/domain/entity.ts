781ad7ac08acdccbde55fb4ea311be4fa7396a7f
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-06-10 00:23:43 -0300

    feat: add some domain definitions

---
+++ src/shared/domain/entity.ts
@@ -0,0 +1,22 @@
+import { UUID } from './value-objects/uuid.vo';
+
+export type BaseModelProps = {
+  id?: string;
+  active?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+};
+
+export class BaseModel implements BaseModelProps {
+  id?: string;
+  active?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+
+  constructor(props: BaseModelProps) {
+    this.id = props?.id || new UUID().get();
+    this.active = props?.active ?? true;
+    this.createdAt = props?.createdAt || new Date();
+    this.updatedAt = props?.updatedAt || new Date();
+  }
+}
---
+++ src/shared/domain/in-memory.repository.ts
@@ -0,0 +1,41 @@
+import { RepositoryContract } from './interfaces/repository.interface';
+
+export class InMemoryBaseRepository<M extends Record<string, any>>
+  implements RepositoryContract<M>
+{
+  items: M[] = [];
+
+  async createOne(entity: M): Promise<M> {
+    this.items.push(entity);
+    return entity;
+  }
+
+  async createMany(entities: M[]): Promise<M[]> {
+    this.items.push(...entities);
+    return this.items.filter((item) =>
+      entities.some((entity) => entity.id === item.id),
+    );
+  }
+
+  async update(entity: M): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === entity.id);
+    if (index !== -1) {
+      this.items[index] = entity;
+    }
+  }
+
+  async delete(entityId: string): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === entityId);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  async findById(entityId: string): Promise<M | undefined> {
+    return this.items.find((item) => item.id === entityId);
+  }
+
+  async findAll(): Promise<M[]> {
+    return [...this.items];
+  }
+}
