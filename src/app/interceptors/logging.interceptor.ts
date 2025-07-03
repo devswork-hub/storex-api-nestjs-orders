0180b81e1941aecd90b6973d84359b3f06f419a2
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-07-02 21:57:57 -0300

    feat: add new interceptors

--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -34,3 +34,23 @@ export class LoggingInterceptor implements NestInterceptor {
     );
   }
 }
+
+// import {
+//   CallHandler,
+//   ExecutionContext,
+//   Injectable,
+//   NestInterceptor,
+// } from '@nestjs/common';
+
+// import { tap } from 'rxjs/operators';
+
+// @Injectable()
+// export class LoggingInterceptor implements NestInterceptor {
+//   intercept(context: ExecutionContext, next: CallHandler<unknown>) {
+//     const date = new Date().toISOString();
+//     const { fieldName } = context.getArgs()[3] ?? { fieldName: 'REST API' };
+//     const message = `${date} Request-Response time of ${fieldName}`;
+//     console.time(message);
+//     return next.handle().pipe(tap(() => console.timeEnd(message)));
+//   }
+// }
