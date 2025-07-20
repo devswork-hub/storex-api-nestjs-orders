f2bfc111e0bc2332de4591f7e084e0e357b30618
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-07-19 22:16:46 -0300

    feat: add graphql load values from config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,37 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
-import { ApolloDriver } from '@nestjs/apollo';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from './config/config.module';
 import { PersistenceModule } from './persistence/persistence.module';
 import { DomainsModule } from '../modules/domain-modules.module';
 import { CustomCacheModule } from './persistence/cache/cache.module';
 import { TypeORMModule } from './persistence/typeorm/typeorm.module';
 import { ScheduleModule } from '@nestjs/schedule';
+import { RabbitmqModule } from './messaging/rabbitmq/rabbitmq.module';
+import { KafkaModule } from './messaging/kafka/kafka.module';
+import { ConfigValues } from './config/config.values';
 
 @Module({
   imports: [
     ScheduleModule.forRoot(),
     ConfigModule.forRoot(),
-    GraphQLModule.forRoot({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      sortSchema: true,
-      introspection: true,
+      inject: [ConfigValues],
+      useFactory: (cfg: ConfigValues) => ({
+        autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        sortSchema: true,
+        introspection: cfg.get('NODE_ENV') === 'development',
+        playground: cfg.get('NODE_ENV') === 'development',
+      }),
     }),
     TypeORMModule,
     PersistenceModule.register({ type: 'mongoose', global: true }),
     CustomCacheModule.forRoot({ isGlobal: true }),
     DomainsModule,
+    RabbitmqModule,
+    KafkaModule,
   ],
 })
 export class AppModule {}
