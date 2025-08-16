7b7a5653bdb6654eb957c5e5366d353e8304e6e6
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-08-15 22:33:29 -0300

    wip: processing non retryable errors

--- src/app/messaging/rabbitmq/rabbitmq-consumer-error.filter.ts
+++ src/app/messaging/rabbitmq/rabbitmq-consumer-error.filter.ts
@@ -1,30 +1,38 @@
+import { ValidationException } from '@/shared/domain/validation/validation-exception';
+import { Nack } from '@golevelup/nestjs-rabbitmq';
 import {
   ArgumentsHost,
   BadRequestException,
   Catch,
   ExceptionFilter,
   Logger,
+  UnprocessableEntityException,
 } from '@nestjs/common';
 
 @Catch()
-export class RabbitMQConsumerErrorFilter<T = any> implements ExceptionFilter {
+export class RabbitMQConsumerErrorFilter implements ExceptionFilter {
   private logger = new Logger(RabbitMQConsumerErrorFilter.name);
 
-  async catch(exception: T, host: ArgumentsHost) {
-    this.logger.error('Erro capturado pelo filter', exception);
+  // acrescentar conforme necessario
+  static readonly NON_RETRYABLE_ERRORS = [
+    ValidationException,
+    BadRequestException,
+    UnprocessableEntityException,
+  ];
 
+  async catch(exception: Error, host: ArgumentsHost) {
     if (host.getType<'rmq'>() !== 'rmq') return;
 
-    const ctx = host.switchToRpc();
-    const message = ctx.getData();
-    const channel = ctx.getContext();
+    const hasRetryableError =
+      RabbitMQConsumerErrorFilter.NON_RETRYABLE_ERRORS.some(
+        (e) => exception instanceof e,
+      );
 
-    if (exception instanceof BadRequestException) {
-      this.logger.debug('===> Tratando BadRequest, ACK message');
-      await channel.ack(message);
-    } else {
-      this.logger.debug('===> Erro genérico, NACK message');
-      await channel.nack(message, false, false);
+    if (hasRetryableError) {
+      this.logger.error(
+        'Possui um erro reprocessavel, jogando pra DLQ ou descartando',
+      );
+      return new Nack(false);
     }
   }
 }
--- src/modules/order/application/messaging/orders.rabbitmq-handler.ts
+++ src/modules/order/application/messaging/orders.rabbitmq-handler.ts
@@ -28,17 +28,9 @@ export class OrdersRabbitMQController {
     routingKey: 'order.created',
   })
   handleEmail(message: any) {
-    try {
-      // o RabbitMQConsumerFilter so ativa quando enviamos um BadRequestException
-      // Usar para cenarios em que eu tenho processamento CRUD dos dados que vierem, e ativar o ZodValidator
-      throw new BadRequestException('Simulando erro');
-      console.log('Emails Queue received:');
-    } catch (error) {
-      if (error instanceof BadRequestException) {
-        // → não devolve para a fila, e se houver DLQ configurada, ela vai para lá
-        this.logger.error('Enviei pra DLQ');
-        throw new Nack(false);
-      }
-    }
+    // o RabbitMQConsumerFilter so ativa quando enviamos erros retryables que defini no filtro
+    // Usar para cenarios em que eu tenho processamento CRUD dos dados que vierem, e ativar o ZodValidator
+    // Aqui pode ser ZodValidator... VadlidationExceptions e outros
+    throw new BadRequestException('Simulando erro');
   }
 }
