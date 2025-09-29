18c23384b70c9c8be8097976d8d4655421e37b3d
deyvisonborges <deyvisonsilvaborges@gmail.com>
2025-09-29 16:26:57 -0300

    fix: resolve filebeat setup

--- src/app/persistence/typeorm/typeorm-datasource.ts
+++ src/app/persistence/typeorm/typeorm-datasource.ts
@@ -12,8 +12,21 @@ export const AppDataSource = new DataSource({
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DB,
   entities: [...typeormEnties],
+  // entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [path.resolve(__dirname, '../../../**/*.migration.{ts,js}')],
+  migrationsTableName: 'migrations',
+  migrationsRun: false,
+  // schema: configService.get<string>('DB_SCHEMA'),
   synchronize: false,
   logging: ['error', 'warn', 'schema'],
   namingStrategy: new SnakeNamingStrategy(),
+  extra: {
+    connectionLimit: 10,
+  },
 });
+
+// @ManyToMany(() => Team, (team) => team.members, {
+//     nullable: true,
+//     eager: false, // Don't automatically load teams with every user query
+//     cascade: false, // Prevent cascading operations to teams
+//   })
